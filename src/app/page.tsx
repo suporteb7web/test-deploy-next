2a796843841517f2e24841fc7fdd135f9f45ab2e
bonieky <bonieky@gmail.com>
2024-07-17 14:43:58 -0300

    edit: header change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Page = async () => {
 
     return (
         <div className="mx-auto max-w-lg">
-            <h1 className="text-xl mb-4">Lista de posts</h1>
+            <h1 className="text-xl mb-4">Lista de posts v1.1</h1>
 
             {allPosts.length === 0 && <PostsEmpty />}
             {allPosts.length > 0 && <PostsList posts={allPosts} />}
